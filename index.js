9da7e57afab21a6a9e733817400dea24e773bc1c
Devesh Kumar Mishra <94969888+devn5900@users.noreply.github.com>
2023-02-24 15:36:57 +0530

    json-server-updated

---
+++ index.js
@@ -0,0 +1,30 @@
+const jsonServer = require("json-server");
+const cors = require("cors");
+const path = require("path");
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
+
+const PORT = 8000;
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
+
+// // server.js
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const router = jsonServer.router('db.json')
+// const middlewares = jsonServer.defaults()
+
+// server.use(middlewares)
+// server.use('', router)
+// server.listen(process.env.PORT || 5000, () => {
+//   console.log('JSON Server is running')
+// })
